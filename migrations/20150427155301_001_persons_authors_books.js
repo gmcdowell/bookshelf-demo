72b480ae5c9f62e5b76fe58e11f1c0ff94e57f5c
Greg McDowell <greg@oxigeno.co.nz>
2016-07-09 14:06:34 -0500

    refactored migration script

--- migrations/20150427155301_001_persons_authors_books.js
+++ migrations/20150427155301_001_persons_authors_books.js
@@ -1,73 +1,58 @@
 'use strict';
 
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
 
-    return Promise.all([
-        // create a Persons table
-        knex.schema.createTable('persons', function(table){
-            table.increments('id');
-            table.string('first_name').notNullable();
-            table.string('family_name').notNullable();
-            table.string('email').notNullable();
-            table.timestamps();
-        })
-            .then(function(){
-                return knex.raw('ALTER TABLE persons ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP');
-            }),
+  return Promise.all([
+    // create a Persons table
+    knex.schema.createTable('person', function (table) {
+      table.increments('id');
+      table.string('first_name').notNullable();
+      table.string('family_name').notNullable();
+      table.string('email').notNullable();
+      table.timestamps(true, true); // use timestamp type and default to current_timestamp
+    }),
 
-        // create an Authors table
-        knex.schema.createTable('authors', function(table){
-            table.increments('id');
-            table.integer('person_id').references('id').inTable('persons');
-            table.timestamps();
-        })
-            .then(function(){
-                return knex.raw('ALTER TABLE authors ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP');
-            }),
+    // create an Authors table
+    knex.schema.createTable('author', function (table) {
+      table.increments('id');
+      table.integer('person_id').references('id').inTable('person');
+      table.timestamps(true, true);
+    }),
 
-        knex.schema.createTable('publishers', function(table){
-            table.increments('id');
-            table.integer('contact_id');
-            table.string('contact_type').defaultTo('persons');
-            table.timestamp('created_at')
-        })
-            .then(function(){
-                return knex.raw('ALTER TABLE publishers ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP');
-            }),
+    knex.schema.createTable('publisher', function (table) {
+      table.increments('id');
+      table.integer('contact_id');
+      table.string('contact_type').defaultTo('person');
+      table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+    }),
 
-        // create a Books table
-        knex.schema.createTable('books', function(table){
-            table.increments('id');
-            table.string('title').notNullable();
-            table.integer('author_id').references('id').inTable('authors');
-            table.integer('publisher_id').references('id').inTable('publishers');
-            table.timestamps();
-        })
-            .then(function(){
-                return knex.raw('ALTER TABLE books ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP');
-            }),
+    // create a Books table
+    knex.schema.createTable('book', function (table) {
+      table.increments('id');
+      table.string('title').notNullable();
+      table.integer('author_id').references('id').inTable('author');
+      table.integer('publisher_id').references('id').inTable('publisher');
+      table.timestamps(true, true);
+    }),
 
-        // create a PersonBooks table
-        knex.schema.createTable('person_books', function(table) {
-            table.increments('id');
-            table.integer('person_id').references('id').inTable('persons').notNullable();
-            table.integer('book_id').references('id').inTable('books').notNullable();
-            table.timestamp('created_at');
-        })
-            .then(function(){
-                return knex.raw('ALTER TABLE person_books ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP');
-            })
-    ]);
+    // create a PersonBooks table
+    knex.schema.createTable('person_book', function (table) {
+      table.increments('id');
+      table.integer('person_id').references('id').inTable('person').notNullable();
+      table.integer('book_id').references('id').inTable('book').notNullable();
+      table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+    })
+  ]);
 
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
 
-    return Promise.all([
-        knex.schema.dropTableIfExists('person_books'),
-        knex.schema.dropTableIfExists('books'),
-        knex.schema.dropTableIfExists('publishers'),
-        knex.schema.dropTableIfExists('authors'),
-        knex.schema.dropTableIfExists('persons')
-    ]);
+  return Promise.all([
+    knex.schema.dropTableIfExists('person_book'),
+    knex.schema.dropTableIfExists('book'),
+    knex.schema.dropTableIfExists('publisher'),
+    knex.schema.dropTableIfExists('author'),
+    knex.schema.dropTableIfExists('person')
+  ]);
 };
