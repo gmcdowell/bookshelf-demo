4a037cd2b933babf57967fd70947c97e66b4d006
Greg McDowell <greg@oxigeno.co.nz>
2016-07-12 13:40:05 -0300

    set up webpack and added build/ to gitignore

--- src/init.js
+++ src/init.js
@@ -2,10 +2,9 @@
  * Created by greg on 12/07/16.
  */
 
-/*
 import App from './server';
 import * as Log from 'winston';
 
 App.listen(App.get('port'), function () {
-  Log.log('API running on port: %s & environment %s', App.get('port'), process.env.NODE_ENV | 'development);
-});*/
+  Log.log('API running on port: %s & environment %s', App.get('port'), process.env.NODE_ENV || 'development');
+});
